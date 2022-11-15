4e18e18b6cb5622a1540e401a87e689f02340184
Ruslan <gimazevs@mail.ru>
2022-11-15 21:01:13 +0300

    втолрое задание

--- Human.java
+++ Human.java
@@ -2,13 +2,15 @@ public class Human {
     int yearOfBirth;
     String name;
     String city;
+    String jobTitle;
 
-    Human(int yearOfBirth, String name, String city) {
+    Human(int yearOfBirth, String name, String city, String jobTitle) {
         this.yearOfBirth = yearOfBirth;
         this.name = name;
         this.city = city;
+        this.jobTitle = jobTitle;
     }
     public void human() {
-        System.out.println("Привет! Меня зовут " + name + " Я из город " + city + "Я родился в " + yearOfBirth + " году. Будем знакомы!");
+        System.out.println("Привет! Меня зовут " + name + "!" + " Я из города " + city + "." + " Я родился в " + yearOfBirth + " году." + " Я работаю на должности " + jobTitle + "." + " Будем знакомы!");
     }
 }
--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Human maksim = new Human(1988, "Максим", "Минск");
-        Human anya = new Human(1993, "Аня","Москва");
-        Human katya = new Human(1992, "Катя", "Калининград");
-        Human artem = new Human(1995, "Артем", "Москва");
+        Human maksim = new Human(1988, "Максим", "Минск","Бренд-менеджер");
+        Human anya = new Human(1993, "Аня","Москва", "Методист образовательных программ");
+        Human katya = new Human(1992, "Катя", "Калининград", "Продакт-менеджер");
+        Human artem = new Human(1995, "Артем", "Москва", "Директор по развитию бизнеса");
         maksim.human();
         anya.human();
         katya.human();
