c81abba2def336e6d7cacfdd7131cb4eeb88c92f
Ruslan <gimazevs@mail.ru>
2022-11-15 20:01:43 +0300

    первый коммит

---
+++ Human.java
@@ -0,0 +1,11 @@
+public class Human {
+    int yearOfBirth;
+    String name;
+    String city;
+
+    Human(int yearOfBirth, String name, String city) {
+        this.yearOfBirth = yearOfBirth;
+        this.name = name;
+        this.city = city;
+    } p
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Cat murzik = new Cat("Мурзик");
+        murzik.age = 1;
+        murzik.meow();
+
+        Cat begemot = new Cat("Бегемот", 5);
+        begemot.meow();
+    }
+}
